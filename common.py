12e21994c5f69d17db3ef9ed76595963db811f1c
Cyrus C <cyrus.castco@gmail.com>
2024-03-19 12:01:51 +0800

    + faster method for pair generation

--- common.py
+++ common.py
@@ -51,3 +51,53 @@ def analyze_alphabets(df: pd.DataFrame):
     print(f"less_than_10: {''.join(less_than_10)}")
     print(f"outliers: {''.join(outliers)}")
     print(f"remainings: {''.join(remainings)}")
+
+
+def change_character(vocab: str, alphabets: list[str]) -> list[str]:
+    new_vocabs = []
+    for i, c in enumerate(vocab):
+        for a in alphabets:
+            if a != c:
+                new_vocab = vocab[:i] + a + vocab[i + 1 :]
+                new_vocabs.append(new_vocab)
+
+    return new_vocabs
+
+
+def character_difference(from_: str, to: str) -> int:
+    count = 0
+    for c1, c2 in zip(from_, to):
+        if c1 != c2:
+            count += 1
+    return count
+
+
+def distance(
+    from_: str,
+    to: str,
+    vocabs_pool: set[str],
+    alphabets: list[str],
+    max_distance: int | None = None,
+) -> tuple[int, list[str]]:
+    if len(from_) != len(to):
+        return (-1, [])
+
+    vocab_path = {from_: ""}
+    queue = [(from_, 0)]
+    while queue:
+        (cur_vocab, cur_distance) = queue.pop(0)
+        if cur_vocab == to:
+            path = [cur_vocab]
+            while path[0] != from_:
+                path.insert(0, vocab_path[path[0]])
+            return (cur_distance, path)
+
+        if cur_distance == max_distance:
+            continue
+
+        for new_vocab in change_character(cur_vocab, alphabets):
+            if (new_vocab not in vocab_path) and (new_vocab in vocabs_pool):
+                vocab_path[new_vocab] = cur_vocab
+                queue.append((new_vocab, cur_distance + 1))
+
+    return (-1, [])
